9609bde794c6a7807ab391aad60086edf2407e54
Jan T. Sott <git@idleberg.com>
2025-10-09 22:40:28 +0200

    chore: use shared config

---
+++ tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig((options) => {
+	const isProduction = options.watch !== true;
+
+	return {
+		target: 'node18',
+		clean: isProduction,
+		dts: isProduction,
+		entry: ['src/index.ts'],
+		format: ['cjs', 'esm'],
+		minify: isProduction,
+	};
+});
